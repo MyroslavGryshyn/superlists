f5086f2bf8ffe6a36ec554c4c70e18e1ddb87495
MyroslavGryshyn <sergiy.1911@gmail.com>
2015-12-08 11:07:02 +0200

    First step of FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import time
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
@@ -90,6 +91,31 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        time.sleep(3)
+        # She noticed that input box is nicely centered
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees the input is centered here
+        # nicely too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=5
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
